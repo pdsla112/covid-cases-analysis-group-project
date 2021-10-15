fa3dfa377e33cbc55f3548c17e2607043e506e50
System Administrator <root@192-168-1-105.tpgi.com.au>
2021-10-15 11:58:47 +1100

    Add relevant starting assignment files

---
+++ assignment.py
@@ -0,0 +1,22 @@
+"""
+Template for the COMP1730/6730 project assignment, S2 2021.
+The assignment specification is available on the course web
+site, at https://cs.anu.edu.au/courses/comp1730/assessment/project/
+
+
+The assignment is due 25/10/2021 at 9:00 am, Canberra time
+
+Collaborators: <list the UIDs of ALL members of your project group here>
+"""
+
+def analyse(path_to_files):
+    pass
+
+
+# The section below will be executed when you run this file.
+# Use it to run tests of your analysis function on the data
+# files provided.
+
+if __name__ == '__main__':
+    # test on folder containg all CSV files
+    analyse('./covid-data')
