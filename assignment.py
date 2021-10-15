d2f994f383646f8e21571f4f2b12e3ebdb45372b
pdsla112@hotmail.com <pdsla112@hotmail.com>
2021-10-16 00:13:52 +1100

    Complete untested code for q1 (a)

--- assignment.py
+++ assignment.py
@@ -26,6 +26,34 @@ if __name__ == '__main__':
     
     
 
+# Question 1 (a):
+def find_most_recent():
+    all_csv = os.listdir("./covid-data")
+    all_csv = [csv_file for csv_file in all_csv if csv_file.endswith(".csv")]
+    latest_csv = max(all_csv)
+    df = pd.read_csv("./covid-data/" + latest_csv)
+    latest_update = max(df["Last_Update"])
+    print(F"""
+          Analysing data from folder...
+          
+          Question 1:
+          Most recent data is in file `{latest_csv}`
+          Last updated at {latest_update}
+          """)
+        
 
-
+# Question 1 (b) (still working on it):
+def find_total_worldwide():
+    all_csv = os.listdir("./covid-data")
+    all_csv = [csv_file for csv_file in all_csv if csv_file.endswith(".csv")]
+    latest_csv = max(all_csv)
+    df = pd.read_csv("./covid-data/" + latest_csv)
+    latest_update = max(df["Last_Update"])
+    print(F"""
+          Analysing data from folder...
+          
+          Question 1:
+          Most recent data is in file `{latest_csv}`
+          Last updated at {latest_update}
+          """)
         
